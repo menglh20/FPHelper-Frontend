ab86ebee5005694ae51af33aa02a36381fe886cb
menglh-mac <2621429138@qq.com>
2025-02-26 14:16:11 +0800

    v1.0.5 简单的须知页面

--- pages/detect/detect.js
+++ pages/detect/detect.js
@@ -1,74 +1,80 @@
 // pages/detect/detect.js
 Page({
-  data: {
-    username: '',
-  },
-  onLoad(options) {
-    const app = getApp()
-    const username = app.globalData.username
-    this.setData({
-      'username': username
-    })
-  },
-  bindViewTakePhoto() {
-    wx.navigateTo({
-      url: '../camera/camera',
-    })
-  },
-  bindViewRecordVideo() {
-    wx.navigateTo({
-      url: '../recorder/recorder',
-    })
-  },
-
-  bindViewUploadPhoto() {
-
-  },
-
-  bindViewUploadVideo() {
-    const that = this; // 保存上下文
-    wx.chooseMedia({
-      count: 1,
-      mediaType: ['video'],
-      sourceType: ['album'], // 仅允许从相册选择
-      compressed: true, // 压缩视频
-      maxDuration: 70, // 设置视频允许的最大时长
-      success(res) {
-        const tempFile = res.tempFiles[0];
-        console.log(tempFile)
-        const tempFilePath = tempFile.tempFilePath; // 视频路径
-        const duration = tempFile.duration; // 视频时长（秒）
-        const sizeMB = tempFile.size / 1024 / 1024; // 视频大小（MB）
-
-        // 校验时长
-        if (duration < 40 || duration > 70) {
-          wx.showToast({
-            title: '视频时长必须在40-70秒之间',
-            icon: 'none',
-          });
-          return;
-        }
-
-        // 校验大小
-        if (sizeMB > 40) {
-          wx.showToast({
-            title: '视频大小不能超过40MB',
-            icon: 'none',
-          });
-          return;
-        }
-
+    data: {
+        username: '',
+    },
+    onLoad(options) {
+        const app = getApp()
+        const username = app.globalData.username
+        this.setData({
+            'username': username
+        })
+    },
+    bindViewTakePhoto() {
         wx.navigateTo({
-          url: `../preview/preview?videoPath=${encodeURIComponent(tempFilePath)}`, // 使用 encodeURIComponent 处理路径
+            url: '../camera/camera',
+        })
+    },
+    bindViewRecordVideo() {
+        wx.navigateTo({
+            url: '../recorder/recorder',
+        })
+    },
+
+    bindViewUploadPhoto() {
+
+    },
+
+    bindViewUploadVideo() {
+        const that = this; // 保存上下文
+        wx.chooseMedia({
+            count: 1,
+            mediaType: ['video'],
+            sourceType: ['album'], // 仅允许从相册选择
+            compressed: true, // 压缩视频
+            maxDuration: 70, // 设置视频允许的最大时长
+            success(res) {
+                const tempFile = res.tempFiles[0];
+                console.log(tempFile)
+                const tempFilePath = tempFile.tempFilePath; // 视频路径
+                const duration = tempFile.duration; // 视频时长（秒）
+                const sizeMB = tempFile.size / 1024 / 1024; // 视频大小（MB）
+
+                // 校验时长
+                if (duration < 40 || duration > 70) {
+                    wx.showToast({
+                        title: '视频时长必须在40-70秒之间',
+                        icon: 'none',
+                    });
+                    return;
+                }
+
+                // 校验大小
+                if (sizeMB > 40) {
+                    wx.showToast({
+                        title: '视频大小不能超过40MB',
+                        icon: 'none',
+                    });
+                    return;
+                }
+
+                wx.navigateTo({
+                    url: `../preview/preview?videoPath=${encodeURIComponent(tempFilePath)}`, // 使用 encodeURIComponent 处理路径
+                });
+            },
+            fail(err) {
+                console.error("用户取消或选择视频失败：", err);
+                wx.showToast({
+                    title: '视频选择失败',
+                    icon: 'none',
+                });
+            }
         });
-      },
-      fail(err) {
-        console.error("用户取消或选择视频失败：", err);
-        wx.showToast({
-          title: '视频选择失败',
-          icon: 'none',
-        });
-      }
-    });
-  },
+    },
+
+    bindViewNotice() {
+        wx.navigateTo({
+            url: '../notice/notice',
+        })
+    },
 })
---
+++ pages/notice/notice.js
@@ -0,0 +1,7 @@
+// pages/notice/notice.js
+Page({
+    // 返回按钮的点击事件
+    goBack: function () {
+      wx.navigateBack(); // 返回上一页
+    },
+  });
